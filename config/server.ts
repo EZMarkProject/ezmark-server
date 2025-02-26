9453bcdffe35493fe2494da27dbff119ed8fb53e
KelvinQiu <kelvinqiu802@outlook.com>
2025-02-26 23:12:03 +0800

    feat: add url

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://csi420-01-vm7.ucd.ie/strapi'),
   app: {
     keys: env.array('APP_KEYS'),
   },
