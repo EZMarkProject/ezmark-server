08651ebbab13905eea321e8aa6e880024a77d430
KelvinQiu <kelvinqiu802@outlook.com>
2025-02-28 13:38:27 +0800

    feat: config jwt expiers time

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,9 @@
-export default () => ({});
+export default () => ({
+    'users-permissions': {
+        config: {
+            jwt: {
+                expiresIn: '5m', // Eg: 60, "45m", "10h", "2 days", "7d", "2y"
+            }
+        }
+    }
+});
