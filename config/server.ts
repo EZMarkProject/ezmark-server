54a6c52e93ec3eefc3eecaf5c2c562f02804ec5f
KelvinQiu <kelvinqiu802@outlook.com>
2025-03-09 23:31:14 +0800

    test: url

--- config/server.ts
+++ config/server.ts
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('NODE_ENV') === 'production'
-    ? env('PUBLIC_URL', 'https://csi420-01-vm7.ucd.ie/strapi')
-    : env('PUBLIC_URL', 'http://localhost:1337'),
+  url: env('PUBLIC_URL', 'https://csi420-01-vm7.ucd.ie/strapi'),
   app: {
     keys: env.array('APP_KEYS'),
   },
