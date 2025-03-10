fad363503209be1d197dd4d7fc56acbe886cbea0
KelvinQiu <kelvinqiu802@outlook.com>
2025-03-10 13:39:00 +0800

    feat: update expiers in 7d

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,7 +2,7 @@ export default () => ({
     'users-permissions': {
         config: {
             jwt: {
-                expiresIn: '5m', // Eg: 60, "45m", "10h", "2 days", "7d", "2y"
+                expiresIn: '7d', // Eg: 60, "45m", "10h", "2 days", "7d", "2y"
             }
         }
     }
