74e233787eb98a8ec4cbb2fe728bb06961325d55
KelvinQiu802 <kelvinqiu802@outlook.com>
2025-03-18 19:22:21 +0800

    feat: add upload config

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,5 +5,15 @@ export default () => ({
                 expiresIn: '7d', // Eg: 60, "45m", "10h", "2 days", "7d", "2y"
             }
         }
+    },
+    upload: {
+        config: {
+            sizeLimit: 1024 * 1024 * 1024, // 1GB
+            providerOptions: {
+                localServer: {
+                    maxage: 300000, // 强制缓存5 minutes
+                }
+            }
+        }
     }
 });
